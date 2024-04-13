61cf2dd536075bf67a58d187ec59dd1f038029b0
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2024-04-13 23:03:44 +0400

    finished section 81

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
+
 function App() {
   const [activePlayer, setActivePlayer] = useState('X');
 
@@ -11,12 +13,13 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName={"Player 1"} symbol={"X"} />
-          <Player initialName={"Player 2"} symbol={"O"} />
+        <ol id="players" className="highlight-player">
+          <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"} />
+          <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} />
         </ol> 
-        <GameBoard onSelectSquare={handleSelectSquare} />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
+      <Log />
     </main>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -11,7 +11,7 @@ export default function Player({ initialName, symbol }) {
     setPlayerName(e.target.value);
   }
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={handleChange} />}
         <span className="player-symbol">{symbol}</span>
