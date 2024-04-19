faeeca0177301fedc9a6b6451f5c645c2912f989
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2024-04-19 20:05:23 +0400

    final polishing

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
 
 
-export default function GameBoard({onSelectSquare, board}) {
+export default function GameBoard({ onSelectSquare, board }) {
 
 
-
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  
-  // function handleSelectSquare(rowIndex, colIndex){
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSquare();
-  // }
-  
   return (
     <ol id='game-board'>
       {board.map((row, rowIndex) => (
@@ -24,7 +11,7 @@ export default function GameBoard({onSelectSquare, board}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
