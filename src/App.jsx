526b1b927d0f0e5421f899b51acf4bbedacf8612
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2024-04-19 15:45:58 +0400

    finished lesson 93.

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,13 @@ function deriveActivePlayer(gameTurns) {
 
 
 function App() {
+
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+
+
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -68,6 +75,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      }
+    })
   }
   return (
     <main>
